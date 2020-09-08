f30ee5a6470910b81f9a7b772fff906cc67e4cff
raulshma <raulshma@gmail.com>
2020-09-08 21:11:57 +0000

    Deploying to gh-pages from  @ 2da707c06c106d27209e4c738de654345eed13c5 🚀

---
+++ precache-manifest.4377bfee1eef21abcb11e1bad6968ddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d645f42068adf449387c62a6945849",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "7425f775af69f36494cc",
+    "url": "/expense-tracker-react/static/css/main.f4fa8f44.chunk.css"
+  },
+  {
+    "revision": "4d86ffcfad1008e27297",
+    "url": "/expense-tracker-react/static/js/2.989e6a1b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/expense-tracker-react/static/js/2.989e6a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7425f775af69f36494cc",
+    "url": "/expense-tracker-react/static/js/main.c71d462c.chunk.js"
+  },
+  {
+    "revision": "da4b07c0500d4ccca1c5",
+    "url": "/expense-tracker-react/static/js/runtime-main.d8148653.js"
+  }
+]);
