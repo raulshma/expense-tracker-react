468dfbec6fe5341951243f9db39882533ff9dc31
raulshma <raulshma@gmail.com>
2020-09-23 17:25:01 +0000

    Deploying to gh-pages from  @ 837ab2b46f2eea936cbe2cd63b053f7c50650d5a 🚀

---
+++ precache-manifest.342dbe560018f68cb945af14545708d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bb714671349c9947ec6a30cbf5839b",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "75eb37dcc4a795869ba4",
+    "url": "/expense-tracker-react/static/css/main.9596fc5c.chunk.css"
+  },
+  {
+    "revision": "f30d0f4b3ab3b040b567",
+    "url": "/expense-tracker-react/static/js/2.ca5a0fbf.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/expense-tracker-react/static/js/2.ca5a0fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75eb37dcc4a795869ba4",
+    "url": "/expense-tracker-react/static/js/main.64db1c6c.chunk.js"
+  },
+  {
+    "revision": "da4b07c0500d4ccca1c5",
+    "url": "/expense-tracker-react/static/js/runtime-main.d8148653.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/expense-tracker-react/precache-manifest.059dd0de8a7b56f146f04caff91aee83.js"
+  "/expense-tracker-react/precache-manifest.342dbe560018f68cb945af14545708d7.js"
 );
 
 self.addEventListener('message', (event) => {
